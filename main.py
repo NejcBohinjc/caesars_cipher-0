08735e919ca072f12380fbb0e9d3cf5c0807622c
NejcBohinjc <nbohinjc@gmail.com>
2024-08-13 18:21:53 +0100

    added the encode function that works

--- main.py
+++ main.py
@@ -37,15 +37,12 @@ def encode(inp,us_in_lc):
 
   new_l = []
   for ele in l0:
-    #first convert to lower case and then upper case
-    if ele.isupper(): # letter is uppercase
-      ele = ele.lower()
     # we convert the letter to ascii
     ord_ele = ord(ele)
     # we shift the ascii value of the letter for the shift value
     ord_ele += shift
     ord_ele = chr(ord_ele)
-    new_l.append(ord_ele.lower())
+    new_l.append(ord_ele)
   
   #join the list together and return the encoded word
   encoded_word = "".join(new_l)
