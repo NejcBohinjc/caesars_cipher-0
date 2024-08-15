36512c0c2c28ea6d0e817a6cebe917958455958d
NejcBohinjc <nbohinjc@gmail.com>
2024-08-15 15:57:37 +0100

    added & changed a couple of lines

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
 # TODO
 # add brute force mode
-# add shift amount as input
+# make sure that the letter go from a-z and don't include other ascii characters
 
 from math import floor
 
+
+def brute_force(user_input_local):
+  print("brute force mode")
+
+  l0 = []
+  for letter in user_input_local:
+    l0.append(letter)
+  
+
+  # this goes trough the whole english alphabet
+  for shift in range(1,26):
+    new_l = []
+    for ele in l0:
+      ord_ele = ord(ele)
+      ord_ele -= shift
+      ord_ele = chr(ord_ele)
+      new_l.append(ord_ele)
+    print(f'{shift}: {"".join(new_l)} \n')
+    
+
+usr_phrase = input("Enter you phrase: ")
 shift = floor(int(input("Enter the shift amount: ")))
-usr_in = input("Enter you phrase: ")
 
 # returns if the function should encode or decode
 def enc_or_dec():
   while True:
-    inp = input("\"enc\" to encode, \"dec\" to decode: ")
+    inp = input("\"enc\" to encode, \"dec\" to decode, brute to brute force: ")
     if inp == "dec" or inp == "enc" or inp == "brute":
       return inp
     else:
@@ -20,6 +40,7 @@ user_input = enc_or_dec()
   
 def encode(user_input_local):
   global user_input
+  global shift
   l0 = []
   for letter in user_input_local:
     l0.append(letter)
@@ -38,17 +59,17 @@ def encode(user_input_local):
 
     ord_ele = chr(ord_ele)
     new_l.append(ord_ele)
-  
+    
   #join the list together and return the encoded word
   encoded_word = "".join(new_l)
   print(encoded_word)
   return encoded_word
 
-def brute_force(user_input_local):
-  print("brute force mode")
 
 
-if usr_in != "brute":
-  encode(usr_in)
+if user_input == "enc" or user_input == "dec":
+  encode(usr_phrase)
+elif user_input == "brute":
+  brute_force(usr_phrase)
 else:
-  brute_force(usr_in)
+  print("something went wrong")
